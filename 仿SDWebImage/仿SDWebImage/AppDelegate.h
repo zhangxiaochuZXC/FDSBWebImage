194f42ca825d29b9d19f5da763cfeeea259109e8
ITManager <zhangjiesharp@163.com>
2017-06-23 15:16:06 +0800

    准备项目文件

---
+++ 仿SDWebImage/仿SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SDWebImage
+//
+//  Created by itcastteacher on 17/6/23.
+//  Copyright © 2017年 itcastteacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿SDWebImage/仿SDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SDWebImage
+//
+//  Created by itcastteacher on 17/6/23.
+//  Copyright © 2017年 itcastteacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
