8b210faeafb79028a71502495c556b9504015ad0
ITManager <zhangjiesharp@163.com>
2017-06-23 17:11:08 +0800

    取消上次正在执行的操作

--- 仿SDWebImage/仿SDWebImage/FDSBWebImage/DownloadOperation.m
+++ 仿SDWebImage/仿SDWebImage/FDSBWebImage/DownloadOperation.m
@@ -48,11 +48,17 @@
     // 模拟网络延迟
     [NSThread sleepForTimeInterval:1.0];
     
+    // 在操作执行的过程中拦截到操作是否被取消了
+    if (self.isCancelled == YES) {
+        NSLog(@"取消 = %@",self.URLString);
+        return;
+    }
+    
     // 图片下载结束,需要使用外界传入的代码块把图片回调给外界
     if (self.finishedBlock != nil) {
-        
         // 回到主线程回调代码块 : 外界的代码块会默认在主线程执行(在哪个线程调用代理方法和发送通知,那么代理方法和通知方法就在哪个线程执行,类似于Block的回调)
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            NSLog(@"完成 = %@",self.URLString);
             self.finishedBlock(image);
         }];
     }
--- 仿SDWebImage/仿SDWebImage/ViewController.m
+++ 仿SDWebImage/仿SDWebImage/ViewController.m
@@ -18,8 +18,12 @@
 @property (nonatomic, strong) NSArray *appList;
 /// 队列
 @property (nonatomic, strong) NSOperationQueue *queue;
+/// 操作缓存池
+@property (nonatomic, strong) NSMutableDictionary *opCache;
 /// 图片控件
 @property (weak, nonatomic) IBOutlet UIImageView *iconImageView;
+/// 记录上一次图片的地址
+@property (nonatomic, copy) NSString *lastURLString;
 
 @end
 
@@ -30,6 +34,8 @@
     
     // 准备队列
     self.queue = [NSOperationQueue new];
+    // 准备操作缓存池
+    self.opCache = [[NSMutableDictionary alloc] init];
     
     [self loadData];
 }
@@ -42,10 +48,30 @@
     // 获取随机的模型
     APPModel *app = self.appList[random];
     
+    // 在建立下载操作前,判断连续传入的图片地址是否一样,如果不一样,就把前一个下载操作取消掉
+    if (![app.icon isEqualToString:_lastURLString] && _lastURLString != nil) {
+        
+        // 取出上一个图片的下载操作,调用cancel方法,取消掉
+        DownloadOperation *lastOP = [self.opCache objectForKey:_lastURLString];
+        [lastOP cancel];
+        
+        // 取消掉的操作,需要从操作缓存池移除
+        [self.opCache removeObjectForKey:_lastURLString];
+    }
+    
+    // 记录上次图片地址
+    _lastURLString = app.icon;
+    
     // 获取随机的图片地址,交给DownloadOperation去下载
     DownloadOperation *op = [DownloadOperation downloadOperationWithURLString:app.icon finished:^(UIImage *image) {
         self.iconImageView.image = image;
+        
+        // 图片下载结束后,移除对应的下载操作
+        [self.opCache removeObjectForKey:app.icon];
     }];
+    
+    // 把操作添加到操作缓存池
+    [self.opCache setObject:op forKey:app.icon];
     
     // 把自定义操作添加到队列
     [self.queue addOperation:op];
